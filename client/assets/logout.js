295dc4ad4d330b41a8edf1d5bf76ebc4c2fdebcc
jgooday48 <joelgooday48@gmail.com>
2023-11-22 18:23:15 +0000

    remove any unessesary code

--- client/assets/logout.js
+++ client/assets/logout.js
@@ -1,4 +1,4 @@
-document.getElementById('log-out').addEventListener("click", async (e) => {
+document.getElementById('log-out').addEventListener("click", async () => {
     fetch('http://localhost:3000/tokens/' + localStorage.getItem("token"), {
         method: 'DELETE',
     })
