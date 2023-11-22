d1b1d12f5ad67ba262b8d7ae3a73e3708bb5b0e9
jgooday48 <joelgooday48@gmail.com>
2023-11-22 18:21:05 +0000

    add more

--- client/assets/logout.js
+++ client/assets/logout.js
@@ -1,11 +1,9 @@
-
-
 document.getElementById('log-out').addEventListener("click", async (e) => {
     fetch('http://localhost:3000/tokens/' + localStorage.getItem("token"), {
         method: 'DELETE',
     })
-        .then(res => res.text())
-        .then(res => console.log(res))
+        .then(result => result.text())
+        .then(result => console.log(result))
 
     localStorage.removeItem("token")
 
